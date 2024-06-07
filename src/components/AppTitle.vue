c0c19c75247243fb8e7d6eb99acf0ab87f624d29
JulioGroaz <julio.groaz2020@gmail.com>
2024-06-07 15:37:34 +0200

    Creato il messaggio 'La  mia prima app con Vue'

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,16 @@
+<script>
+    export default{
+        data(){
+            return{
+                mesage:'La mia prima app con Vite!'
+            }
+        }
+    }
+</script>
+
+<template>
+    <h1>{{ message }}</h1>
+
+<style scoped>
+
+</style>
