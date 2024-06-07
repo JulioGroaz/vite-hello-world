3797a9d7184a4370eb5aae4e39bca52f1c03dc7b
JulioGroaz <julio.groaz2020@gmail.com>
2024-06-07 15:57:45 +0200

    Corretto errori che non mi permettevano di mostrare la scritta

--- src/components/AppTitle.vue
+++ src/components/AppTitle.vue
@@ -1,16 +1,18 @@
-<script>
-    export default{
-        data(){
-            return{
-                mesage:'La mia prima app con Vite!'
-            }
-        }
-    }
-</script>
-
 <template>
     <h1>{{ message }}</h1>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            message: 'La mia prima app con Vite!'
+        }
+    }
+}
+</script>
 
 <style scoped>
 
 </style>
+
